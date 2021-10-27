63d7907189170c4ac158a8003894fa9104099cb7
horsicq <horsicq@gmail.com>
2021-10-27 19:19:38 +0200

    Fix: 2021-10-27

--- xarchives.cmake
+++ xarchives.cmake
@@ -3,6 +3,6 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 include(${CMAKE_CURRENT_LIST_DIR}/xarchive.cmake)
 
 set(XARCHIVES_SOURCES
-    ${XARCHIVE_SOURCES}
+    ${XARCHIVES_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xarchives.cpp
 )
