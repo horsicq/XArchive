29ec16a7fc7dc79e9821f0dee6cdde26985a2127
horsicq <horsicq@gmail.com>
2021-10-27 20:37:06 +0200

    Fix: 2021-10-27

--- xarchive.cmake
+++ xarchive.cmake
@@ -3,9 +3,10 @@ include_directories(${CMAKE_CURRENT_LIST_DIR}/3rdparty/bzip2/src/)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/3rdparty/lzma/src/)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/3rdparty/zlib/src/)
 
-# TODO
+include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xbinary.cmake)
 
 set(XARCHIVE_SOURCES
+    ${XBINARY_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xarchive.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xmachofat.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xsevenzip.cpp
