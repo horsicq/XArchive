516cdfb573c40436d84b91b4b9f6987899bbae6c
horsicq <horsicq@gmail.com>
2021-11-07 00:51:43 +0100

    Fix: 2021-11-07

--- xarchive.cmake
+++ xarchive.cmake
@@ -1,4 +1,3 @@
-# TODO guard
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 include_directories(${CMAKE_CURRENT_LIST_DIR}/3rdparty/bzip2/src/)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/3rdparty/lzma/src/)
