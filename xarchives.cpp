2afa1208419f7397d90aafe24c96a305e61687ca
horsicq <horsicq@gmail.com>
2022-10-26 15:41:20 +0200

    Fix: 2022-10-26

--- xarchives.cpp
+++ xarchives.cpp
@@ -37,7 +37,7 @@ QList<XArchive::RECORD> XArchives::getRecords(QIODevice *pDevice,qint32 nLimit,X
     QList<XArchive::RECORD> listResult;
 
     // TODO more !!!
-    // CAB RAR
+    // CAB RAR 7ZIP
     QSet<XBinary::FT> stFileTypes=XBinary::getFileTypes(pDevice,true);
 
     if(stFileTypes.contains(XArchive::FT_ZIP))
