de8f06595037bd710938f5b60e135c241c68cb19
horsicq <horsicq@gmail.com>
2023-02-27 19:15:30 +0100

    Fix: 2023-02-27

--- x_ar.h
+++ x_ar.h
@@ -29,13 +29,13 @@ class X_Ar : public XArchive {
 #pragma pack(push)
 #pragma pack(1)
     struct FRECORD {
-        char fileId[16];    // File identifier	ASCII
-        char fileMod[12];   // File modification timestamp (in seconds)	Decimal
-        char ownerId[6];    // Owner ID	Decimal
-        char groupId[6];    // Group ID	Decimal
-        char fileMode[8];   // File mode (type and permission)	Octal
-        char fileSize[10];  // File size in bytes	Decimal
-        char endChar[2];    // Ending characters	0x60 0x0A
+        char fileId[16];    // File identifier                          ASCII
+        char fileMod[12];   // File modification timestamp (in seconds) Decimal
+        char ownerId[6];    // Owner ID                                 Decimal
+        char groupId[6];    // Group ID                                 Decimal
+        char fileMode[8];   // File mode (type and permission)          Octal
+        char fileSize[10];  // File size in bytes                       Decimal
+        char endChar[2];    // Ending characters 0x60 0x0A
     };
 #pragma pack(pop)
 
