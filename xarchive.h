12d5d97f1d1df028e9429af04494d2ee55461193
horsicq <horsicq@gmail.com>
2023-02-28 18:50:03 +0100

    Fix: 2023-02-28

--- xarchive.h
+++ xarchive.h
@@ -46,7 +46,7 @@ public:
         COMPRESS_METHOD_LZH5,
         COMPRESS_METHOD_LZH6,
         COMPRESS_METHOD_LZH7
-        // TODO check more
+        // TODO more methods
     };
 
     struct RECORD {
