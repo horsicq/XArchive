1f6f5d9e6bb0d707f57a1e71b836c4cfa82a528d
horsicq <horsicq@gmail.com>
2023-11-05 22:41:47 +0100

    Update file(s): 2023-11-05

--- xcompress.h
+++ xcompress.h
@@ -23,6 +23,18 @@
 
 #include <QObject>
 
+#undef SCHAR_MIN
+#define SCHAR_MIN (-SCHAR_MAX - 1)
+#undef SCHAR_MAX
+#define SCHAR_MAX __SCHAR_MAX__
+
+#undef UCHAR_MAX
+#if __SCHAR_MAX__ == __INT_MAX__
+# define UCHAR_MAX (SCHAR_MAX * 2U + 1U)
+#else
+# define UCHAR_MAX (SCHAR_MAX * 2 + 1)
+#endif
+
 class XCompress {
 public:
 #define MAXMATCH 256 /* Maximum match length. */
