a5d41ad5eab5f8906d5edb97bcdc3910cb1c79d2
horsicq <horsicq@gmail.com>
2024-03-02 00:03:03 +0100

    Update file(s): 2024-03-02

--- xarchives.cmake
+++ xarchives.cmake
@@ -5,4 +5,5 @@ include(${CMAKE_CURRENT_LIST_DIR}/xarchive.cmake)
 set(XARCHIVES_SOURCES
     ${XARCHIVE_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xarchives.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xarchives.h
 )
