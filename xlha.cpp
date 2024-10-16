c66632ffd4ac3f1afeebc0e8fd812bb7504f787c
horsicq <horsicq@gmail.com>
2024-10-16 23:44:51 +0200

    Update file(s): 2024-10-16

--- xlha.cpp
+++ xlha.cpp
@@ -147,9 +147,10 @@ XBinary::_MEMORY_MAP XLHA::getMemoryMap(MAPMODE mapMode, PDSTRUCT *pPdStruct)
 {
     Q_UNUSED(mapMode)
 
-    PDSTRUCT pdStructEmpty = XBinary::createPdStruct();
+    XBinary::PDSTRUCT pdStructEmpty = {};
 
     if (!pPdStruct) {
+        pdStructEmpty = XBinary::createPdStruct();
         pPdStruct = &pdStructEmpty;
     }
 
