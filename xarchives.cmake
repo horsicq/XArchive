95afde0e227d50a6450a18fae95cd2ac0e8354f4
horsicq <horsicq@gmail.com>
2024-12-04 08:38:51 +0100

    Update file(s): 2024-12-04

--- xarchives.cmake
+++ xarchives.cmake
@@ -1,8 +1,12 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/xarchive.cmake)
+if (NOT DEFINED XARCHIVE_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/xarchive.cmake)
+    set(XARCHIVES_SOURCES ${XARCHIVES_SOURCES} ${XARCHIVE_SOURCES})
+endif()
 
 set(XARCHIVES_SOURCES
+    ${XARCHIVES_SOURCES}
     ${XARCHIVE_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xarchives.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xarchives.h
