d61e16de66fed24d55329461b0187e93ab55d1f5
horsicq <horsicq@gmail.com>
2025-06-25 00:46:29 +0200

    Update file(s): 2025-06-25

--- Compress/xlzh.h
+++ Compress/xlzh.h
@@ -23,6 +23,21 @@
 
 #include <QObject>
 
+#ifdef Q_OS_LINUX
+#if (QT_VERSION_MAJOR > 5)
+#undef SCHAR_MIN
+#define SCHAR_MIN (-SCHAR_MAX - 1)
+#undef SCHAR_MAX
+#define SCHAR_MAX __SCHAR_MAX__
+#undef UCHAR_MAX
+#if __SCHAR_MAX__ == __INT_MAX__
+#define UCHAR_MAX (SCHAR_MAX * 2U + 1U)
+#else
+#define UCHAR_MAX (SCHAR_MAX * 2 + 1)
+#endif
+#endif
+#endif
+
 class XLZH : public QObject
 {
     Q_OBJECT
